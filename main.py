8dd7860718d8bee63d61ad0338b1ed814db30e74
saurabh <kapanarinsal6@gmail.com>
2024-02-12 22:47:13 +0530

    it is what it is

---
+++ main.py
@@ -0,0 +1,12 @@
+# To find the rank of word in the dictionary given the word has no repetion of letters
+
+import math
+
+word = list(input("enter your word: "))
+ascending = sorted(word) # permutaion and combination the word is first sorted
+for i in ascending.index(word[0]):
+    # i dont know how to do this 
+    break
+
+
+
